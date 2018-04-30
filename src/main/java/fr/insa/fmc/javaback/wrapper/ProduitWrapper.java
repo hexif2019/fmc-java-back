0bd41fbe95f1c4d049cb7c85f28aafcdbe6d1607
Pikavenger <hugomartin45@gmail.com>
2018-04-30 09:45:22 +0200

    ProduitWarp
    Pti

---
+++ src/main/java/fr/insa/fmc/javaback/wrapper/ProduitWrapper.java
@@ -0,0 +1,16 @@
+package fr.insa.fmc.javaback.wrapper;
+
+public class ProduitWrapper {
+
+    private String denomination;
+    private String description;
+    private int nb;
+    private int prix;
+    private int poids;
+    private String img;
+    private int volume;
+    private int longueur;
+    private int largeur;
+    private int hauteur;
+
+}
