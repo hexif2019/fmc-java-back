1423042dde41bf9f04eb5f712d274ac8f9f4dab7
Pikavenger <hugomartin45@gmail.com>
2018-04-30 11:39:47 +0200

    Mise a jour commande

--- src/main/java/fr/insa/fmc/javaback/controller/CommandeController.java
+++ src/main/java/fr/insa/fmc/javaback/controller/CommandeController.java
@@ -1,15 +1,18 @@
 package fr.insa.fmc.javaback.controller;
 
 import fr.insa.fmc.javaback.entity.Commande;
+import fr.insa.fmc.javaback.entity.MagasinsCommande;
 import fr.insa.fmc.javaback.entity.ProduitsCommande;
 import fr.insa.fmc.javaback.entity.enums.enumEtatCommande;
 import fr.insa.fmc.javaback.repository.ClientRepository;
 import fr.insa.fmc.javaback.repository.CommandeRepository;
 import fr.insa.fmc.javaback.wrapper.CommandeWrapper;
+import fr.insa.fmc.javaback.wrapper.MagasinWrapper;
 import fr.insa.fmc.javaback.wrapper.UserWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -43,8 +46,29 @@ public class CommandeController {
         //TODO: verifier le prix plus loin dans la mise a jour
         commande.setIdClient(commandeWrap.getUserid());
 
+        ArrayList<MagasinsCommande> listMag = new ArrayList<MagasinsCommande>();
 
-        //commande.setMagasinsCommande(commandeWrap.getMagasinsCommande());
+        for(int i = 0; i<commandeWrap.getMagasins().size(); i++) {
+
+            MagasinsCommande magasin = new MagasinsCommande();
+            magasin.setId((long) i);
+            MagasinWrapper magasinWrap = commandeWrap.getMagasins().get(i);
+            magasin.setAdresse(magasinWrap.getAdresse());
+            magasin.setDescription(magasinWrap.getDescription());
+            magasin.setDenomination(magasinWrap.getDenomination());
+            magasin.setEmail(magasinWrap.getEmail());
+            magasin.setPosition(magasinWrap.getPosition());
+            magasin.setIdMagasin(magasinWrap.getId());
+            magasin.setImg(magasinWrap.getImg());
+
+            for (int j = 0; j < magasinWrap.getProduits().size(); j++) {
+                ProduitsCommande produit = new ProduitsCommande();
+                //TODO: finir d ajouter element produit et l ajouter a la liste
+            }
+            listMag.add(magasin);
+        }
+
+        commande.setMagasinsCommande(listMag);
 
 
 
