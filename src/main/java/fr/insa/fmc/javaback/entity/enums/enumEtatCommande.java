d4eb88786bde38ec67e53b9248d1ec1b36dd5c54
Pikavenger <hugomartin45@gmail.com>
2018-05-01 14:44:40 +0200

    Modif enum Commande

--- src/main/java/fr/insa/fmc/javaback/entity/enums/enumEtatCommande.java
+++ src/main/java/fr/insa/fmc/javaback/entity/enums/enumEtatCommande.java
@@ -3,15 +3,15 @@ package fr.insa.fmc.javaback.entity.enums;
 public enum enumEtatCommande {
     EDITION,
     PAYEMENT_EFFECTUE,
-    ATTENTE_CONFIRMATION_MAGASIN,
     ANNULATION_CLIENT,
     ANNULATION_SYSTEM,
     ANNULATION_MAGASIN,             //Si tout les magasins ont annule ou timeout
     VALIDE_MAGASINS_MAIS_MODIF,    //Si un des magasins n a pas valide ou indique ne pas avoir un article, cas le plus chiant a traite
+    VALIDE_MAGASIN,                //valide par tout les magasins sans soucis
     ATTRIBUE_A_COURSIER,
     EN_COURS_DE_LIVRAISON,
     DANS_CASIER,
     RECUPERE_CLIENT,
-    CASIER_TIMEOUT             //Le client n'a pas validé avoir récupéré la commande, et le delai de recuperation de la commande est depasse
-    //RESTE: evacuer(la commande a étais suprimer du casier)
+    CASIER_TIMEOUT,             //Le client n'a pas validé avoir récupéré la commande, et le delai de recuperation de la commande est depasse
+    VIDE_PAR_NOUS               //apres un timeout, lorsque l on a tout enleve
 }
--- src/main/java/fr/insa/fmc/javaback/entity/enums/enumEtatMagasinCommande.java
+++ src/main/java/fr/insa/fmc/javaback/entity/enums/enumEtatMagasinCommande.java
@@ -3,15 +3,15 @@ package fr.insa.fmc.javaback.entity.enums;
 public enum enumEtatMagasinCommande {
     EDITION,
     PAYEMENT_EFFECTUE,
-    ATTENTE_CONFIRMATION_MAGASIN,
     ANNULATION_CLIENT,
     ANNULATION_SYSTEM,
     ANNULATION_MAGASIN,
     VALIDE_MAGASINS_MAIS_MODIF,    //Si le magasin valide mais n'a pas tout les produits, seul etat qui n est pas un c/c de la commande en soit ?
+    VALIDE_MAGASIN,                 //Si le magasin valide tout les produits sans soucis
     ATTRIBUE_A_COURSIER,
     EN_COURS_DE_LIVRAISON,
     DANS_CASIER,
     RECUPERE_CLIENT,
-    CASIER_TIMEOUT
-    //RESTE: evacuer(la commande a étais suprimer du casier)
+    CASIER_TIMEOUT,
+    VIDE_PAR_NOUS
 }
